f53359be7197df425ae3b4aa9391483d003b5a40
Mary Lin <themarylin90@gmail.com>
2018-07-08 01:38:16 -0700

    twitter analysis

---
+++ config.py
@@ -0,0 +1,5 @@
+#Twitter API keys
+consumer_key ="idkPXmIRMPtWzcp08mJIRojO1"
+consumer_secret="5bb4x3PwJ5Zn8MIbINXV3w6ffPzxvBiipjgcFAx1wBxxnHKMHi"
+access_token="822488123321921538-4HM00cXp7IdkgwNjT7XI3QykJaRujb1"
+access_token_secret="rdwsjI7E63GvVbnM7coVTrBQo8ujn3XJkh07XnJR0dLZE"
